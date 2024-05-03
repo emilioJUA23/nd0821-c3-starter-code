86a535422298f6e25c0c35dfa6741838a5122cf3
luisJuarezVana <110055835+luisJuarezVana@users.noreply.github.com>
2024-05-03 17:28:00 -0600

    base clase and starter app written.

--- starter/main.py
+++ starter/main.py
@@ -1,1 +1,32 @@
 # Put the code for your API here.
+from fastapi import FastAPI, HTTPException
+from typing import Union, Optional
+# BaseModel from Pydantic is used to define data objects
+from pydantic import BaseModel
+import pandas as pd
+import os, pickle
+from starter.ml.data import process_data
+
+
+class InputData(BaseModel):
+    age: int
+    workclass: str 
+    fnlgt: int
+    education: str
+    education_num: int
+    marital_status: str
+    occupation: str
+    relationship: str
+    race: str
+    sex: str
+    capital_gain: int
+    capital_loss: int
+    hours_per_week: int
+    native_country: str
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,10 +1,14 @@
 # Script to train machine learning model.
-
 from sklearn.model_selection import train_test_split
-
+from  starter.ml.model import train_model, save_model  
+import pandas as pd
+from starter.ml.data import process_data
 # Add the necessary imports for the starter code.
 
 # Add code to load in the data.
+data_path = '../../data/census.csv'
+model_path = '../../model/'
+data = pd.read_csv(data_path)
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
@@ -26,3 +30,9 @@ X_train, y_train, encoder, lb = process_data(
 # Proces the test data with the process_data function.
 
 # Train and save a model.
+
+model = train_model(X_train, y_train)
+
+model_path = '../model/'
+save_model(model, f"{model_path}model.pkl")
+
